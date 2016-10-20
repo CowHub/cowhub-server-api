1c0e438cea89405eba3c3cdae194c64e072ed9cf
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-10-20 22:09:53 +0100

    Add name

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ docker rmi -f $(docker images | grep -v 'REPOSITORY' | grep -v 'cowhub-server-ap
 
 # Bring service up (with latest)
 docker build -t cowhub-server-api .
-docker run -d -p 80:80 -p 443:443 -i cowhub-server-api
+docker run -d -p 80:80 -p 443:443 --name cowhub-server-api -i cowhub-server-api
