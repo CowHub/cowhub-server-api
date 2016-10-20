17937013cb753e07e6414bde61dbf331b5b6d2c2
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-10-20 20:57:31 +0100

    Improved deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 
 # Build latest image
 cd /app
-docker build -t cowhub-server-api .
 
 # Take service down
-docker kill cowhub-server-api
+docker rm -f $(docker ps -a -q)
+docker rmi -f $(docker images -q)
 
 # Bring service up (with latest)
+docker build -t cowhub-server-api .
 docker run -d -p 80:80 -i cowhub-server-api
