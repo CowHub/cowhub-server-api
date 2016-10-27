8b3d3e1a1e630d9cf2ab66206d480b4024166f0c
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-10-27 15:42:40 +0100

    Add validation and update scripts

--- app/models/cattle.rb
+++ app/models/cattle.rb
@@ -1,7 +1,10 @@
 class Cattle < ActiveRecord::Base
   belongs_to :user
 
-  validates :individual_number, uniqueness: { scope: [:country_code, :herdmark, :check_digit] }
+  validates :country_code, null: false
+  validates :herdmark, null: false
+  validates :check_digit, null: false
+  validates :individual_number, uniqueness: { scope: [:country_code, :herdmark, :check_digit] }, null: false
 
   def generate_tag
     "#{country_code}#{format('%06d', herdmark)}#{check_digit}#{format('%05d', individual_number)}"
