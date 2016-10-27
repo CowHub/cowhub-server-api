5aec58592f777682cb177b88bb114e3fdffaea17
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-10-27 21:02:17 +0100

    Check required fields are filled

--- spec/models/cattle_spec.rb
+++ spec/models/cattle_spec.rb
@@ -11,9 +11,24 @@ RSpec.describe Cattle, type: :model do
     expect(cattle).to_not be_valid
   end
 
-  it 'is not valid without a herdmark'
-  it 'is not valid without a check_digit'
-  it 'is not valid without an individual_number'
+  it 'is not valid without a herdmark' do
+    cattle = FactoryGirl.build(:cattle)
+    cattle.herdmark = nil
+    expect(cattle).to_not be_valid
+  end
+
+  it 'is not valid without a check_digit' do
+    cattle = FactoryGirl.build(:cattle)
+    cattle.check_digit = nil
+    expect(cattle).to_not be_valid
+  end
+
+  it 'is not valid without an individual_number' do
+    cattle = FactoryGirl.build(:cattle)
+    cattle.individual_number = nil
+    expect(cattle).to_not be_valid
+  end
+
   it 'is not valid with an invalid country_code'
   it 'is not valid with an invalid herdmark'
   it 'is not valid with an invalid check_digit'
