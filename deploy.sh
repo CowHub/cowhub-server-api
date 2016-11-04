77d1ec0782fdbae38e8a74a888361379efaba70a
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-04 01:24:43 +0000

    Hotfix v3 deployment

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,4 @@ docker rm -f $(docker ps -a -q)
 # Bring service up
 docker-compose -f /docker-compose-production.yml pull
 docker-compose -f /docker-compose-production.yml up -d
+docker-compose -f /docker-compose-production.yml run api bundle exec rake db:migrate
