ae7d9aee05f05db5215cd5d96643e2ac83f4fcd9
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-04 00:18:29 +0000

    Rewrite deployment

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-# Build latest image
-cd /app
-
 # Take service down
 docker rm -f $(docker ps -a -q)
-docker rmi -f $(docker images | grep -v 'REPOSITORY' | grep -v 'cowhub-server-api' | grep -v 'ruby' | awk -F ' ' '{ print $3 }')
 
-# Bring service up (with latest)
-docker build -t cowhub-server-api .
-docker run -d --env-file '/.env' -p 80:8080 --name cowhub-server-api -i cowhub-server-api
+# Bring service up
+docker-compose up -d -f /docker-compose-production.yml --force-recreate
