2dc72c35d83c8d1f06129cf6a1fb5cd3c9c160d5
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-04 01:11:14 +0000

    Hotfix v0 deployment

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@
 docker rm -f $(docker ps -a -q)
 
 # Bring service up
-docker-compose up -d -f /docker-compose-production.yml --force-recreate
+docker-compose pull
+docker-compose up -d -f /docker-compose-production.yml
