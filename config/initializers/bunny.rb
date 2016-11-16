0b3684b3366c3aea32bf8950cb451992324106e1
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-16 17:48:32 +0000

    Refactor of bunny initialiser

--- config/initializers/bunny.rb
+++ config/initializers/bunny.rb
@@ -1,6 +1,6 @@
 require 'bunny'
 
-bunny = Bunny.new(host: ENV['RABBIT_HOST'])
-bunny.start
+bunny = Bunny.new(host: ENV['RABBIT_HOST']).start
 channel = bunny.create_channel
+
 Rails.application.config.task_queue = channel.queue('task_queue', durable: true)
