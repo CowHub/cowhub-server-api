13af715149689253977abaa1e59aa78a26dddd09
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-30 23:03:57 +0000

    Initialise eb with application name

--- scripts/deploy_image_to_elasticbeanstalk.sh
+++ scripts/deploy_image_to_elasticbeanstalk.sh
@@ -18,5 +18,5 @@ aws elasticbeanstalk create-application-version --region=$REGION --application-n
 
 echo "UPDATING APPLICATION VERSION..."
 cd scripts
-eb init -r $REGION -a $AWS_APPLICATION_NAME
+eb init $AWS_APPLICATION_NAME -r $REGION
 eb deploy -l $DOCKER_TAG
