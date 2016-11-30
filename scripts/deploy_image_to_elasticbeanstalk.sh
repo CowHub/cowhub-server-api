b84d54a0b186d8dc3f73cd06efa9ad86e72a610d
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-30 18:01:40 +0000

    Add deployment of application

--- scripts/deploy_image_to_elasticbeanstalk.sh
+++ scripts/deploy_image_to_elasticbeanstalk.sh
@@ -5,5 +5,6 @@ DOCKERRUN_FILE=$DOCKER_TAG-Dockerrun.aws.json
 EB_BUCKET=$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY
 
 # Run aws command to create a new EB application with label
-aws elasticbeanstalk create-application-version --region=$REGION --application-name $AWS_APPLICATION_NAME 
+aws elasticbeanstalk create-application-version --region=$REGION --application-name $AWS_APPLICATION_NAME
     --version-label $DOCKER_TAG --source-bundle S3Bucket=$DEPLOYMENT_BUCKET,S3Key=$BUCKET_DIRECTORY/$DOCKERRUN_FILE
+aws elasticbeanstalk update-application-version --application-name $AWS_APPLICATION_NAME --version-label $DOCKER_TAG
