5c4587dbc7704b374691801bb148f5c3a4f7c786
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-30 13:23:04 +0000

    Validate match has image

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,8 @@
 class Match < ApplicationRecord
   belongs_to :user
   belongs_to :cattle, optional: true
+
   enum status: [:pending, :found, :not_found]
+
+  validates :image_uri, presence: true
 end
