831ba091a8e31dcc346179f9b9f687184f8a7dd1
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-30 21:16:27 +0000

    Add debugging messages

--- scripts/deploy_image_to_elasticbeanstalk.sh
+++ scripts/deploy_image_to_elasticbeanstalk.sh
@@ -4,11 +4,6 @@ DOCKER_TAG=$1
 DOCKERRUN_FILE=$DOCKER_TAG-Dockerrun.aws.json
 EB_BUCKET=$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY
 
-# Print Dockerrun file
-echo "DOCKER RUN FILE:"
-cat ./scripts/$DOCKERRUN_FILE
-echo ""
-
 # Print env variables for debugging
 echo "REGION:\t$REGION"
 echo "AWS_APPLICATION_NAME:\t$AWS_APPLICATION_NAME"
@@ -17,8 +12,10 @@ echo "DEPLOYMENT_BUCKET:\t$DEPLOYMENT_BUCKET"
 echo "BUCKET_DIRECTORY:\t$BUCKET_DIRECTORY"
 echo "DOCKERRUN_FILE:\t$DOCKERRUN_FILE"
 
+echo "CREATING APPLICATION VERSION..."
 # Run aws command to create a new EB application with label
 aws elasticbeanstalk create-application-version --region=$REGION --application-name $AWS_APPLICATION_NAME --version-label $DOCKER_TAG --source-bundle S3Bucket=$DEPLOYMENT_BUCKET,S3Key=$BUCKET_DIRECTORY/$DOCKERRUN_FILE
 
+echo "UPDATING APPLICATION VERSION..."
 # Deploy application
 aws elasticbeanstalk update-application-version --region=$REGION --application-name $AWS_APPLICATION_NAME --version-label $DOCKER_TAG
