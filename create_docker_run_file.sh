86872022a883c6836bafde61e9cc64dc9e000d4a
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-30 17:32:25 +0000

    Added scripts for deployment

---
+++ create_docker_run_file.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+DOCKER_TAG=$1
+
+# Prefix of file name is the tag.
+DOCKERRUN_FILE=$DOCKER_TAG-Dockerrun.aws.json
+
+# Replacing tags in the file and creating a file.
+sed -e "s/<TAG>/$DOCKER_TAG/" -e "s/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/" -e "s/<IMAGE_NAME>/$IMAGE_NAME/" -e "s/<EXPOSED_PORTS>/$EXPOSED_PORTS/" -e "s/<AUTHENTICATION_KEY>/$AUTHENTICATION_KEY/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+S3_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/$DOCKERRUN_FILE"
+
+# Uploading json file to $S3_PATH
+aws s3 cp $DOCKERRUN_FILE $S3_PATH
---
+++ upload_image_to_elasticbeanstalk.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+DOCKER_TAG=$1
+
+DOCKERRUN_FILE=$DOCKER_TAG-Dockerrun.aws.json
+EB_BUCKET=$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY
+
+# Run aws command to create a new EB application with label
+aws elasticbeanstalk create-application-version --region=$REGION --application-name $AWS_APPLICATION_NAME 
+    --version-label $DOCKER_TAG --source-bundle S3Bucket=$DEPLOYMENT_BUCKET,S3Key=$BUCKET_DIRECTORY/$DOCKERRUN_FILE
