e42fdc410dee41cad91e4b1ff3b570c1ca113801
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-11-30 17:52:08 +0000

    Added production npm

--- scripts/create_docker_run_file.sh
+++ scripts/create_docker_run_file.sh
@@ -5,7 +5,7 @@ DOCKER_TAG=$1
 DOCKERRUN_FILE=$DOCKER_TAG-Dockerrun.aws.json
 
 # Replacing tags in the file and creating a file.
-sed -e "s/<TAG>/$DOCKER_TAG/" -e "s/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/" -e "s/<IMAGE_NAME>/$IMAGE_NAME/" -e "s/<EXPOSED_PORTS>/$EXPOSED_PORTS/" -e "s/<AUTHENTICATION_KEY>/$AUTHENTICATION_KEY/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+sed -e "s/<TAG>/$DOCKER_TAG/" -e "s/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/" -e "s/<IMAGE_NAME>/$IMAGE_NAME/" -e "s/<EXPOSED_PORTS>/$EXPOSED_PORTS/" -e "s/<AUTHENTICATION_KEY>/$AUTHENTICATION_KEY/" < ./scripts/Dockerrun.aws.json.template > $DOCKERRUN_FILE
 S3_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/$DOCKERRUN_FILE"
 
 # Uploading json file to $S3_PATH
