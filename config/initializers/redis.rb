f883248c81c28178962055ea9a8f0444737b68c8
KarimNahas <kn1114@ic.ac.uk>
2016-11-30 20:18:40 +0000

    Initialized redis in a new thread

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -13,17 +13,19 @@ else
   )
 end
 
-# redis.subscribe('match_result_#{Rails.application.config.server_id}') do |on|
-#   on.message do |channel, message|
-#     return unless channel == 'match_result_#{Rails.application.config.server_id}'
-#     response = JSON.parse(message)
-#     match = Match.find_by(id: response.request_id)
-#     return unless match
-#     if response.diff < ENV['DIFF_THRESHOLD']
-#       match.update_attribute(status: 'not_found')
-#     else
-#       match.update_attribute(status: 'found')
-#       match.update_attribute(cattle_id: response.cattle_id)
-#     end
-#   end
-# end
+Thread.new do
+  redis.subscribe('match_result_#{Rails.application.config.server_id}') do |on|
+    on.message do |channel, message|
+      return unless channel == 'match_result_#{Rails.application.config.server_id}'
+      response = JSON.parse(message)
+      match = Match.find_by(id: response.request_id)
+      return unless match
+      if response.diff < ENV['DIFF_THRESHOLD']
+        match.update_attribute(status: 'not_found')
+      else
+        match.update_attribute(status: 'found')
+        match.update_attribute(cattle_id: response.cattle_id)
+      end
+    end
+  end
+end
