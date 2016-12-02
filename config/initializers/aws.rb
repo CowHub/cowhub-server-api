d018bf4e57529494529463b8b6808d4de1787c99
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-12-02 14:21:53 +0000

    Correct hash style

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -11,11 +11,11 @@ if ENV['RAILS_ENV'] == 'production'
   }
 else
   access_configuration = {
-    :access_key_id => 'YOUR_ACCESS_KEY_ID',
-    :secret_access_key => 'YOUR_SECRET_ACCESS_KEY',
-    :s3_endpoint => 'localhost',
-    :s3_port => 10001,
-    :use_ssl => false
+    access_key_id: 'YOUR_ACCESS_KEY_ID',
+    secret_access_key: 'YOUR_SECRET_ACCESS_KEY',
+    s3_endpoint: 'localhost',
+    s3_port: 10001,
+    use_ssl: false
   }
 end
 
