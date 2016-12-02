706cba5b3d77f4321bce7c274f350a12436cfd0e
KarimNahas <kn1114@ic.ac.uk>
2016-12-02 17:35:02 +0000

    Added Match model

---
+++ db/migrate/20161202171000_create_matches.rb
@@ -0,0 +1,11 @@
+class CreateMatches < ActiveRecord::Migration[5.0]
+  def change
+    create_table :matches do |t|
+      t.references :user, foreign_key: true
+      t.references :cattle, foreign_key: true
+      t.string :image_uri
+      t.integer :status
+      t.timestamps
+    end
+  end
+end
