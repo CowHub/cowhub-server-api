12ffaa55b21fa32f8891c461c9d4e50a64500ec4
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-12-02 15:19:11 +0000

    Implement s3 upload for images

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -4,7 +4,6 @@ Aws.config.update(
   region: 'eu-west-1'
 )
 
-access_configuration = {}
 if ENV['RAILS_ENV'] == 'production'
   access_configuration = {
     access_key_id: ENV['AWS_ACCESS_KEY'],
@@ -14,9 +13,16 @@ else
   access_configuration = {
     access_key_id: 'key',
     secret_access_key: 'secret',
-    endpoint: 'http://s3:10001',
+    endpoint: 'http://s3:4569',
     force_path_style: true
   }
 end
 
 $s3 = Aws::S3::Client.new(access_configuration)
+
+unless ENV['RAILS_ENV'] == 'production'
+  $s3.create_bucket(
+    acl: 'private',
+    bucket: 'cowhub-production-images'
+  )
+end
