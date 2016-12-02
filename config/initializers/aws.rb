c299a4f929af2dcfc52d510f5490c5dd51842ca0
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2016-12-02 14:21:16 +0000

    Use fakes3 in non-production environments

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,8 +1,22 @@
-if ENV['RAILS_ENV'] == 'production'
-  require 'aws-sdk'
+require 'aws-sdk'
 
-  $s3 = Aws::S3::Client.new(
+Aws.config.update(
+  region: 'eu-west-1'
+)
+
+if ENV['RAILS_ENV'] == 'production'
+  access_configuration = {
     access_key_id: ENV['AWS_ACCESS_KEY'],
     secret_access_key: ENV['AWS_SECRET_KEY']
-  )
+  }
+else
+  access_configuration = {
+    :access_key_id => 'YOUR_ACCESS_KEY_ID',
+    :secret_access_key => 'YOUR_SECRET_ACCESS_KEY',
+    :s3_endpoint => 'localhost',
+    :s3_port => 10001,
+    :use_ssl => false
+  }
 end
+
+$s3 = Aws::S3::Client.new(access_configuration)
