278e4ec5f67bf43d569d94142e4672fb59f0f258
KarimNahas <kn1114@ic.ac.uk>
2016-12-20 23:14:59 +0200

    Moved configuration to initializer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,16 +75,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  # This handles cross-origin resource sharing.
-  # See: https://github.com/cyu/rack-cors
-  config.middleware.insert_before 0, "Rack::Cors" do
-    allow do
-      origins 'd3b2ewp13q9ixf.cloudfront.net'
-
-      # Reminder: On the following line, the 'methods' refer to the 'Access-
-      # Control-Request-Method', not the normal Request Method.
-      resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put, :patch], credentials: true
-    end
-  end
 end
---
+++ config/initializers/rack_cors.rb
@@ -0,0 +1,16 @@
+# Be sure to restart your server when you modify this file.
+
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+
+# Read more: https://github.com/cyu/rack-cors
+# This handles cross-origin resource sharing.
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # In development, we don't care about the origin.
+    origins ENV['RAILS_ENV'] == 'production' ? 'd3b2ewp13q9ixf.cloudfront.net' : '*'
+    # Reminder: On the following line, the 'methods' refer to the 'Access-
+    # Control-Request-Method', not the normal Request Method.
+    resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put, :patch], credentials: true
+  end
+end
