fede6425ebdc52ad4ec1e42a671e34547df34e76
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-01-04 14:14:38 +0000

    Update rack_cors.rb

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # In development, we don't care about the origin.
-    origins { ENV['RAILS_ENV'] == 'production' ? 'd3b2ewp13q9ixf.cloudfront.net' : '*' }
+    origins { ENV['RAILS_ENV'] == 'production' ? '*.cowhub.co.uk', 'cowhub.co.uk' : '*' }
     # Reminder: On the following line, the 'methods' refer to the 'Access-
     # Control-Request-Method', not the normal Request Method.
     resource '*', headers: :any, methods: [:get, :post, :options, :delete, :put, :patch], credentials: true
