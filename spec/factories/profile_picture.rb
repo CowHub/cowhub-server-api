61efd8a4ecbd092aac0d69fc17c9f52aa4a5e4e1
KarimNahas <kn1114@ic.ac.uk>
2017-01-07 23:21:36 +0000

    Changed bucket name in profile picture factory

--- spec/factories/profile_picture.rb
+++ spec/factories/profile_picture.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :profile_picture do
     cattle { FactoryGirl.create(:cattle) }
     image_uri do
-      image_uri = "cattle/#{cattle.user.id}/#{cattle.id}/#{Faker::Number.between(1, 999)}-imprint-original"
+      image_uri = "cattle/#{cattle.user.id}/#{cattle.id}/#{Faker::Number.between(1, 999)}-profile-original"
       $s3.put_object(
         acl: 'private',
         body: SecureRandom.base64,
