7668fd4264607c3ffa4f963f58a24616d011341f
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-01-09 23:18:09 +0000

    Added lock for match

--- app/controllers/lambda_controller.rb
+++ app/controllers/lambda_controller.rb
@@ -19,25 +19,30 @@ class LambdaController < ApplicationController
     # Params: id, value, image_id
     match = Match.find_by(id: params[:id])
     if match
-      match.results += 1
-      match.save
+      match.with_lock do
+        match.results += 1
+        match.save
+      end
 
       # No match found
       if params[:value] == 'inf'
         render status: :ok
         return
       end
-      current_value = match.value
-      image = ImprintImage.find_by(id: params[:image_id])
-      if Float(params[:value]) < current_value && image
-        match.imprint_image_id = image.id
-        match.value = Float(params[:value])
-      end
-      if match.valid?
-        match.save
-        render status: :ok
-      else
-        render json: { error: match.errors.full_messages }, status: :bad_request
+
+      match.with_lock do
+        current_value = match.value
+        image = ImprintImage.find_by(id: params[:image_id])
+        if Float(params[:value]) < current_value && image
+          match.imprint_image_id = image.id
+          match.value = Float(params[:value])
+        end
+        if match.valid?
+          match.save
+          render status: :ok
+        else
+          render json: { error: match.errors.full_messages }, status: :bad_request
+        end
       end
     else
       render status: :not_found
