c9ec27235103ce3da6abce906c2fe41c29879fd5
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-01-09 05:01:58 +0000

    Attempt #2 Fix script

--- scripts/create_docker_run_file.sh
+++ scripts/create_docker_run_file.sh
@@ -3,7 +3,7 @@ DOCKER_TAG=$1
 
 # Prefix of file name is the tag.
 SOURCE_BUNDLE=$DOCKER_TAG
-S3_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/$SOURCE_BUNDLE"
+S3_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/$SOURCE_BUNDLE.zip"
 
 # Print env variables for debugging
 echo "DOCKER_TAG:\t$DOCKER_TAG"
@@ -16,8 +16,9 @@ echo "AUTHENTICATION_KEY:\t$AUTHENTICATION_KEY"
 sed -e "s/<TAG>/$DOCKER_TAG/" -e "s/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/" -e "s/<IMAGE_NAME>/$IMAGE_NAME/" -e "s/<EXPOSED_PORTS>/$EXPOSED_PORTS/" -e "s/<AUTHENTICATION_KEY>/$AUTHENTICATION_KEY/" < ./scripts/Dockerrun.aws.json.template > ./scripts/Dockerrun.aws.json
 
 # Bundle
-cd scripts && cp -r ../.ebextensions ./
-cd scripts && zip -r $SOURCE_BUNDLE.zip .ebextensions Dockerrun.aws.json
+cd scripts
+cp -r ../.ebextensions ./
+zip -r $SOURCE_BUNDLE.zip .ebextensions Dockerrun.aws.json
 
 # Uploading json file to $S3_PATH
-aws s3 cp ./scripts/$SOURCE_BUNDLE.zip $S3_PATH
+aws s3 cp $SOURCE_BUNDLE.zip $S3_PATH
