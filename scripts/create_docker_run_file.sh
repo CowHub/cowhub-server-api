8d6ea939481243b8b735d8dd92c0b38c11a8a66c
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2017-01-09 04:53:04 +0000

    Attempt #1 to deploy with ebextensions

--- scripts/create_docker_run_file.sh
+++ scripts/create_docker_run_file.sh
@@ -2,8 +2,8 @@
 DOCKER_TAG=$1
 
 # Prefix of file name is the tag.
-DOCKERRUN_FILE=$DOCKER_TAG-Dockerrun.aws.json
-S3_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/$DOCKERRUN_FILE"
+SOURCE_BUNDLE=$DOCKER_TAG
+S3_PATH="s3://$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY/$SOURCE_BUNDLE"
 
 # Print env variables for debugging
 echo "DOCKER_TAG:\t$DOCKER_TAG"
@@ -13,7 +13,11 @@ echo "EXPOSED_PORTS:\t$EXPOSED_PORTS"
 echo "AUTHENTICATION_KEY:\t$AUTHENTICATION_KEY"
 
 # Replacing tags in the file and creating a file.
-sed -e "s/<TAG>/$DOCKER_TAG/" -e "s/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/" -e "s/<IMAGE_NAME>/$IMAGE_NAME/" -e "s/<EXPOSED_PORTS>/$EXPOSED_PORTS/" -e "s/<AUTHENTICATION_KEY>/$AUTHENTICATION_KEY/" < ./scripts/Dockerrun.aws.json.template > ./scripts/$DOCKERRUN_FILE
+sed -e "s/<TAG>/$DOCKER_TAG/" -e "s/<DEPLOYMENT_BUCKET>/$DEPLOYMENT_BUCKET/" -e "s/<IMAGE_NAME>/$IMAGE_NAME/" -e "s/<EXPOSED_PORTS>/$EXPOSED_PORTS/" -e "s/<AUTHENTICATION_KEY>/$AUTHENTICATION_KEY/" < ./scripts/Dockerrun.aws.json.template > ./scripts/Dockerrun.aws.json
+
+# Bundle
+cd scripts && cp -r ../.ebextensions ./
+cd scripts && zip -r $SOURCE_BUNDLE.zip .ebextensions Dockerrun.aws.json
 
 # Uploading json file to $S3_PATH
-aws s3 cp ./scripts/$DOCKERRUN_FILE $S3_PATH
+aws s3 cp ./scripts/$SOURCE_BUNDLE.zip $S3_PATH
--- scripts/deploy_image_to_elasticbeanstalk.sh
+++ scripts/deploy_image_to_elasticbeanstalk.sh
@@ -2,7 +2,7 @@
 DOCKER_TAG=$1
 
 VERSION_LABEL=$DOCKER_TAG
-DOCKERRUN_FILE=$VERSION_LABEL-Dockerrun.aws.json
+SOURCE_BUNDLE=$VERSION_LABEL.zip
 EB_BUCKET=$DEPLOYMENT_BUCKET/$BUCKET_DIRECTORY
 
 # Print env variables for debugging
@@ -11,11 +11,11 @@ echo "AWS_APPLICATION_NAME:\t$AWS_APPLICATION_NAME"
 echo "DOCKER_TAG:\t$DOCKER_TAG"
 echo "DEPLOYMENT_BUCKET:\t$DEPLOYMENT_BUCKET"
 echo "BUCKET_DIRECTORY:\t$BUCKET_DIRECTORY"
-echo "DOCKERRUN_FILE:\t$DOCKERRUN_FILE"
+echo "SOURCE_BUNDLE:\t$SOURCE_BUNDLE"
 
 echo "CREATING APPLICATION VERSION..."
 # Run aws command to create a new EB application with label
-aws elasticbeanstalk create-application-version --region=$REGION --application-name $AWS_APPLICATION_NAME --version-label $VERSION_LABEL --source-bundle S3Bucket=$DEPLOYMENT_BUCKET,S3Key=$BUCKET_DIRECTORY/$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --region=$REGION --application-name $AWS_APPLICATION_NAME --version-label $VERSION_LABEL --source-bundle S3Bucket=$DEPLOYMENT_BUCKET,S3Key=$BUCKET_DIRECTORY/$SOURCE_BUNDLE
 
 echo "UPDATING APPLICATION VERSION..."
 cd scripts && eb init $AWS_APPLICATION_NAME -r $REGION && eb deploy -l $VERSION_LABEL
